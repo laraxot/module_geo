0bbeafe3dcce4bc8067e20a00885c43633813145
marco xot <marco.sottana@gmail.com>
2019-09-15 18:11:23 +0200

    .

---
+++ Transformers/GeoJsonResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Geo\Transformers;
+/*
+*  GEOJSON e' uno standard
+* https://it.wikipedia.org/wiki/GeoJSON
+**/
+
+//use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GeoJsonResource extends JsonResource{
+
+	public function toArray($request){
+        return [
+        	'type'=>'Feature',
+        	'properties'=>[
+        		'p'=>'vending_machine',
+        		'id'=>'node/31605830',
+        	],
+        	'geometry'=>[
+            	'type' => 'Point',
+            	'coordinates' => [round($this->longitude,7),round($this->latitude,7)],
+        	]
+        ];
+    }
+}
+
+/*
+{"type":"Feature","properties":{"p":"vending_machine","id":"node/31605830"},"geometry":{"type":"Point","coordinates":[9.0796524,48.5308688]
+*/

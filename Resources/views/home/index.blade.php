b44fd5751901a48d5fc4145b748e5355fb2b54cd
marco76tv <marco.sottana@gmail.com>
2019-09-17 23:13:02 +0200

    .

--- Resources/views/home/index.blade.php
+++ Resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
 	{{--
 	<script src="{{asset('data/farmshopGeoJson.js')}}"></script>
 	--}}
-	<div class="progress" id="map_progress">
+	<div class="progress" id="map_progress" style="height: 50px;">
   		<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
   			<span id="current-progress"></span>
   		</div>

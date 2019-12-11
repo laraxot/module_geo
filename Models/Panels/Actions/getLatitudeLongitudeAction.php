97512dbc1d98e5431bf3318ee3ed2a4be8725b2e
marco76tv <marco.sottana@gmail.com>
2019-12-11 12:38:00 +0100

    .

---
+++ Models/Panels/Actions/getLatitudeLongitudeAction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Modules\Geo\Models\Panels\Actions;
+
+//-------- models -----------
+
+//-------- services --------
+use Modules\Xot\Services\ArrayService;
+use Modules\Xot\Services\ImportService;
+
+//-------- bases -----------
+use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
+
+
+class getLatitudeLongitudeAction extends XotBasePanelAction {
+    public $name = 'get_lat_lng'; //name for calling Action
+    public $onContainer = true; //onlyContainer
+    public $icon = '<i class="fas fa-magic"></i><i class="fas fa-map-marked-alt"></i>';
+
+    public function handle() {
+        $rows=$this->rows
+        	->whereRaw('latitude is null or longitude is null')
+        	->inRandomOrder()
+        	->get();
+        foreach($rows as $row){
+        	$address=$row->getAddress();
+        	$addr_arr=ImportService::getAddressFields(['address'=>$address]);
+        	$row->fill($addr_arr);
+        	$row->save();
+
+        }
+        
+        //return ArrayService::toXls(['data' => $data, 'filename' => 'test']);
+    }
+}

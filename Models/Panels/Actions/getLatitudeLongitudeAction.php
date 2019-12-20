4841fd0841f7a9d42914e257847a0ef2547186a2
marco76tv <marco.sottana@gmail.com>
2019-12-20 15:59:41 +0100

    .

--- Models/Panels/Actions/getLatitudeLongitudeAction.php
+++ Models/Panels/Actions/getLatitudeLongitudeAction.php
@@ -5,12 +5,10 @@ namespace Modules\Geo\Models\Panels\Actions;
 //-------- models -----------
 
 //-------- services --------
-use Modules\Xot\Services\ArrayService;
-use Modules\Xot\Services\ImportService;
-
-//-------- bases -----------
 use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
-
+use Modules\Xot\Services\ArrayService;
+//-------- bases -----------
+use Modules\Xot\Services\ImportService;
 
 class getLatitudeLongitudeAction extends XotBasePanelAction {
     public $name = 'get_lat_lng'; //name for calling Action
@@ -18,22 +16,20 @@ class getLatitudeLongitudeAction extends XotBasePanelAction {
     public $icon = '<i class="fas fa-magic"></i><i class="fas fa-map-marked-alt"></i>';
 
     public function handle() {
-        $rows=$this->rows
-        	->whereRaw('latitude is null or longitude is null')
-        	->inRandomOrder()
-        	->get();
-        foreach($rows as $row){
-        	$address=$row->getAddress();
-        	try{
-        		$addr_arr=ImportService::getAddressFields(['address'=>$address,'id'=>$row->id]);
-        		$row->fill($addr_arr);
-        		$row->save();
-        	}catch(\Exception $e){
-
-        	}
-
+        $rows = $this->rows
+            ->whereRaw('latitude is null or longitude is null')
+            ->inRandomOrder()
+            ->get();
+        foreach ($rows as $row) {
+            $address = $row->getAddress();
+            try {
+                $addr_arr = ImportService::getAddressFields(['address' => $address, 'id' => $row->id]);
+                $row->fill($addr_arr);
+                $row->save();
+            } catch (\Exception $e) {
+            }
         }
-        
+
         //return ArrayService::toXls(['data' => $data, 'filename' => 'test']);
     }
 }

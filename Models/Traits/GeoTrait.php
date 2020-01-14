a72c1e955a062c82657df18d5106223d4db0e71f
marco76tv <marco.sottana@gmail.com>
2020-01-14 19:01:40 +0100

    .

--- Models/Traits/GeoTrait.php
+++ Models/Traits/GeoTrait.php
@@ -5,18 +5,17 @@ namespace Modules\Geo\Models\Traits;
 //---- services --
 use Modules\Geo\Services\GeoService;
 
+trait GeoTrait {
+    public function scopeWithDistance($query, $lat, $lng) {
+        $q = $query;
+        if (0 != $lat && 0 != $lng) {
+            $haversine = GeoService::haversine($lat, $lng);
 
-trait GeoTrait{
+            return $query->selectRaw("*,{$haversine} AS distance")
+                        ->orderBy('distance')
+                        ;
+        }
 
-
-	public function scopeWithDistance($query,$lat,$lng){
-		$q = $query;
-		if($lat != 0 && $lng != 0){
-			$haversine=GeoService::haversine($lat,$lng);
-			return $query->selectRaw("*,{$haversine} AS distance")
-						->orderBy('distance')
-						;
-		}
-		return $q;
-	}
+        return $q;
+    }
 }

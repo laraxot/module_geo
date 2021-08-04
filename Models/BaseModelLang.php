cdc8e9c1e84c6d68db4c8c203d6768413285acfd
Malebestia <malebestia@yahoo.it>
2021-08-04 19:32:31 +0200

    .

--- Models/BaseModelLang.php
+++ Models/BaseModelLang.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 //use Laravel\Scout\Searchable;
 
 use Illuminate\Database\Eloquent\Model;
-use Modules\Blog\Models\Traits\LinkedTrait;
+use Modules\Xot\Models\Traits\LinkedTrait;
 use Modules\Xot\Contracts\ModelContract;
 use Modules\Xot\Traits\Updater;
 

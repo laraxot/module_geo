db1c352b656875c0d0b346b19b2d7af056069bdd
Malebestia <malebestia@yahoo.it>
2021-10-16 11:00:22 +0200

    .

---
+++ Models/City.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Geo\Models;
+
+//--------- models --------
+
+//--- services
+
+/**
+ * Modules\Geo\Models\City.
+ */
+class City extends BaseModelLang {
+    /**
+     * @var string[]
+     */
+    protected $fillable = ['id'];
+
+    //---------- RELATIONSHIPS -----------
+}

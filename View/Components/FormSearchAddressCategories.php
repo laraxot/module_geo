0390d5090ce2028b2d7faee834ab536dbf1073f2
marco76tv <marco.sottana@gmail.com>
2021-10-21 23:09:31 +0200

    .

--- View/Components/FormSearchAddressCategories.php
+++ View/Components/FormSearchAddressCategories.php
@@ -6,15 +6,18 @@ namespace Modules\Geo\View\Components;
 
 use Illuminate\View\Component;
 
-class FormSearchAddressCategories extends Component
-{
+class FormSearchAddressCategories extends Component {
     /**
      * Undocumented function.
      *
      * @return void
      */
-    public function render()
-    {
-        return '<div>PRESIII</div>';
+    public function render() {
+        $view = 'geo::components.form_search_address_categories';
+        $view_params = [
+            'view' => $view,
+        ];
+
+        return view()->make($view, $view_params);
     }
 }

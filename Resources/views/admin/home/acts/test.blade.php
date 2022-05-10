49f297fe6b2cafe3118e67419f9ea2023c17105c
marco76tv <marco.sottana@gmail.com>
2022-05-10 09:41:13 +0200

    .

--- Resources/views/admin/home/acts/test.blade.php
+++ Resources/views/admin/home/acts/test.blade.php
@@ -1,6 +1,13 @@
 @extends('adm_theme::layouts.app')
 @section('content')
+<<<<<<< HEAD
     <livewire:test class="pluto">
         <x-slot name="slot1"> nonna papera </x-slot>
     </livewire:test>
 @endsection
+=======
+<livewire:geo::test class="pluto">
+    <x-slot name="slot1"> nonna papera </x-slot>
+</livewire:geo::test>
+@endsection
+>>>>>>> bbe83faf (first)
--- Resources/views/components/form_search_address_categories.blade.php
+++ Resources/views/components/form_search_address_categories.blade.php
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 <livewire:form-search-address-categories :attributes="$attributes" :slot="$slot" />
+=======
+<livewire:geo::form-search-address-categories :attributes="$attributes" :slot="$slot" />
+>>>>>>> bbe83faf (first)

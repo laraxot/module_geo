0751284be0e23ba2eb2e100c551831face24e427
Davide Cavallini <davidecavallini1987@gmail.com>
2022-05-17 11:37:29 +0200

    .

---
+++ Resources/js/components/map/Map3.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+      <div>
+      CIAO
+      </div>
+  </section>
+</template>
+
+<script>
+
+/*import $ from "./leaflet.js";
+import $ from "./leaflet.permalink.min.js";
+import $ from "jQuery";
+import $ from "./leaflet.extra-markers.min.js";
+import $ from "./leaflet.markercluster.js";
+import $ from "./leaflet-sidebar.js";
+import $ from "./opening_hours+deps.min.js";
+import $ from "./popupcontent.js";
+import $ from "./direktvermarkter.js";*/
+
+export default  {
+    name: 'Map3',
+
+    components: {
+        
+    },
+    data () {
+        return {
+        };
+    },
+
+    created () {
+
+        
+    },
+    mounted() {
+
+           
+    }
+ };
+</script>
+
+<style scoped  lang="scss">
+    /*@import "~leaflet.extra-markers.min.css";*/
+</style>

91462d36cb4f3a2ff6a111ea91a2b25c85339221
marco76tv <marco.sottana@gmail.com>
2022-09-13 09:34:31 +0200

    up

--- View/Components/FormSearchAddressCategories.php
+++ View/Components/FormSearchAddressCategories.php
@@ -10,54 +10,11 @@ use Illuminate\View\Component;
 class FormSearchAddressCategories extends Component {
     /**
      * Undocumented function.
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> d4fc524 (rebase)
-=======
->>>>>>> cd852c9 (rebase)
-=======
->>>>>>> 4cc354ba (up)
      */
     public function render(): Renderable {
         /**
          * @phpstan-var view-string
          */
-<<<<<<< HEAD
-=======
-     *
-     */
-    public function render():Renderable {
-        /** 
-        * @phpstan-var view-string
-        */
-<<<<<<< HEAD
->>>>>>> f2b13f11 (.)
-=======
-     */
-    public function render(): Renderable {
-        /**
-         * @phpstan-var view-string
-         */
->>>>>>> 9de2ec4b (up)
-=======
->>>>>>> f2b13f1 (.)
-<<<<<<< HEAD
->>>>>>> d4fc524 (rebase)
-=======
-=======
-     */
-    public function render(): Renderable {
-        /**
-         * @phpstan-var view-string
-         */
->>>>>>> 9de2ec4 (up)
->>>>>>> cd852c9 (rebase)
-=======
->>>>>>> 4cc354ba (up)
         $view = 'geo::components.form_search_address_categories';
         $view_params = [
             'view' => $view,

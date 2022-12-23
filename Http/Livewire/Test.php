47e7a00d3de1c3a64a2eee92fd25688a05f6b222
marco xot <marco.sottana@gmail.com>
2022-12-23 17:31:49 +0100

    up

--- Http/Livewire/Test.php
+++ Http/Livewire/Test.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace Modules\Geo\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Contracts\Support\Renderable;
 
 class Test extends Component {
     /*
@@ -17,7 +18,7 @@ class Test extends Component {
      */
     // public $lookup;
 
-    public function render() {
+    public function render():Renderable {
         $view = 'geo::livewire.test';
         $view_params = [
             'view' => $view,

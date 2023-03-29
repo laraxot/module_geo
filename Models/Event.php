0065f2cb650bbde3977eb9bc31c2217493963d3e
marco xot <marco.sottana@gmail.com>
2023-03-29 11:05:10 +0200

    up

--- Models/Event.php
+++ Models/Event.php
@@ -14,44 +14,42 @@ use Modules\Geo\Models\Traits\HasPlaceTrait;
 /**
  * Modules\Geo\Models\Event.
  *
- * @property int                                                                      $post_id
- * @property \Illuminate\Support\Carbon|null                                          $created_at
- * @property \Illuminate\Support\Carbon|null                                          $updated_at
- * @property \Illuminate\Support\Carbon|null                                          $date_start
- * @property \Illuminate\Support\Carbon|null                                          $date_end
- * @property string|null                                                              $created_by
- * @property string|null                                                              $updated_by
- * @property string|null                                                              $deleted_by
- * @property string|null                                                              $guid
- * @property string|null                                                              $image_src
- * @property string|null                                                              $lang
- * @property string|null                                                              $post_type
- * @property string|null                                                              $subtitle
- * @property string|null                                                              $title
- * @property string|null                                                              $txt
- * @property string|null                                                              $user_handle
- * @property \Modules\Geo\Models\Place|null                                           $place
- * @property \Illuminate\Database\Eloquent\Collection<int, \Modules\Geo\Models\Place> $places
- * @property int|null                                                                 $places_count
- * @property \Modules\Lang\Models\Post|null                                           $post
- * @property \Illuminate\Database\Eloquent\Collection<int, \Modules\Lang\Models\Post> $posts
- * @property int|null                                                                 $posts_count
- * @property mixed                                                                    $url
- *
- * @method static \Illuminate\Database\Eloquent\Builder|Event         newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Event         newQuery()
+ * @property int $post_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $date_start
+ * @property \Illuminate\Support\Carbon|null $date_end
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property string|null $deleted_by
+ * @property string|null $guid
+ * @property string|null $image_src
+ * @property-read string|null $lang
+ * @property-read string|null $post_type
+ * @property string|null $subtitle
+ * @property string|null $title
+ * @property string|null $txt
+ * @property-read string|null $user_handle
+ * @property-read \Modules\Geo\Models\Place|null $place
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Modules\Geo\Models\Place> $places
+ * @property-read int|null $places_count
+ * @property-read \Modules\Lang\Models\Post|null $post
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Modules\Lang\Models\Post> $posts
+ * @property-read int|null $posts_count
+ * @property-write mixed $url
+ * @method static \Illuminate\Database\Eloquent\Builder|Event newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Event newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang ofItem(string $guid)
- * @method static \Illuminate\Database\Eloquent\Builder|Event         query()
- * @method static \Illuminate\Database\Eloquent\Builder|Event         whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event         whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event         whereDateEnd($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event         whereDateStart($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event         whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event         wherePostId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event         whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event         whereUpdatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereDateEnd($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereDateStart($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereDeletedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event wherePostId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereUpdatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang withPost(string $guid)
- *
  * @mixin \Eloquent
  */
 class Event extends BaseModelLang {

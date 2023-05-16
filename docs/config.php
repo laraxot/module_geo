9d556f3a976bf34a9f1eb0e816d0d6f969cceaaf
marco76tv <marco.sottana@gmail.com>
2023-05-16 09:50:11 +0200

    up

--- docs/config.php
+++ docs/config.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Support\Str;
 
 $moduleName = 'Geo';
@@ -16,12 +14,16 @@ return [
     'collections' => [
         'posts' => [
             'path' => function ($page) {
-                return $page->lang.'/posts/'.Str::slug($page->getFilename());
+                //return $page->lang.'/posts/'.Str::slug($page->getFilename());
+                //return 'posts/' . ($page->featured ? 'featured/' : '') . Str::slug($page->getFilename());
+
+                return 'posts/'.Str::slug($page->getFilename());
             },
         ],
         'docs' => [
             'path' => function ($page) {
-                return $page->lang.'/docs/'.Str::slug($page->getFilename());
+                //return $page->lang.'/docs/'.Str::slug($page->getFilename());
+                return 'docs/'.Str::slug($page->getFilename());
             },
         ],
     ],
@@ -37,18 +39,29 @@ return [
     'isActive' => function ($page, $path) {
         return Str::endsWith(trimPath($page->getPath()), trimPath($path));
     },
+    'isItemActive' => function ($page, $item) {
+        return Str::endsWith(trimPath($page->getPath()), trimPath($item->getPath()));
+    },
     'isActiveParent' => function ($page, $menuItem) {
         if (is_object($menuItem) && $menuItem->children) {
             return $menuItem->children->contains(function ($child) use ($page) {
                 return trimPath($page->getPath()) == trimPath($child);
             });
         }
+    },/*
+    'url' => function ($page, $path) {
+        return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
     },
+    */
     'url' => function ($page, $path) {
         if (Str::startsWith($path, 'http')) {
             return $path;
         }
-        // return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
-        return url('/'.$page->lang.'/'.trimPath($path));
+         //return url('/'.$page->lang.'/'.trimPath($path));
+        return url('/'.trimPath($path));
+    },
+
+    'children' => function ($page, $docs) {
+        return $docs->where('parent_id', $page->id);
     },
 ];

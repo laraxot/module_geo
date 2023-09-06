07af3a962e73caccb437a24e9e5eb6805306ab5a
dade1987 <dade1987@users.noreply.github.com>
2023-09-06 11:25:22 +0000

    Check & fix styling

--- Models/Panels/CountryPanel.php
+++ Models/Panels/CountryPanel.php
@@ -1,26 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Geo\Models\Panels;
 
+use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Modules\Cms\Contracts\RowsContract;
-use Illuminate\Contracts\Support\Renderable;
-
-
 use Modules\Cms\Models\Panels\XotBasePanel;
 
-class CountryPanel extends XotBasePanel {
+class CountryPanel extends XotBasePanel
+{
     /**
      * The model the resource corresponds to.
-     *
-     * @var string
      */
     public static string $model = 'Modules\Geo\Models\Country';
 
     /**
      * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
      */
     public static string $title = 'title';
 
@@ -29,24 +26,24 @@ class CountryPanel extends XotBasePanel {
      *
      * @var array
      */
-    public static $search = array (
-);
+    public static $search = [
+    ];
 
     /**
      * The relationships that should be eager loaded on index queries.
-     *
      */
-    public function with():array {
+    public function with(): array
+    {
         return [];
     }
 
-    public function search() :array {
-
+    public function search(): array
+    {
         return [];
     }
 
     /**
-     * on select the option id
+     * on select the option id.
      *
      * quando aggiungi un campo select, è il numero della chiave
      * che viene messo come valore su value="id"
@@ -55,11 +52,13 @@ class CountryPanel extends XotBasePanel {
      *
      * @return int|string|null
      */
-    public function optionId($row) {
+    public function optionId($row)
+    {
         $key = $row->getKey();
-        if(null===$key||(!is_string($key)&&!is_int($key))){
+        if (null === $key || (! is_string($key) && ! is_int($key))) {
             throw new \Exception('['.__LINE__.']['.class_basename(__CLASS__).']');
         }
+
         return $key;
     }
 
@@ -68,14 +67,16 @@ class CountryPanel extends XotBasePanel {
      *
      * @param \Modules\Geo\Models\Country $row
      */
-    public function optionLabel($row):string {
+    public function optionLabel($row): string
+    {
         return 'To Set';
     }
 
     /**
      * index navigation.
      */
-    public function indexNav(): ?Renderable {
+    public function indexNav(): ?Renderable
+    {
         return null;
     }
 
@@ -88,76 +89,67 @@ class CountryPanel extends XotBasePanel {
      */
     public function indexQuery(array $data, $query)
     {
-        //return $query->where('user_id', $request->user()->id);
+        // return $query->where('user_id', $request->user()->id);
         return $query;
     }
-
-
 
     /**
      * Get the fields displayed by the resource.
      *
      * @return array
-        'col_size' => 6,
-        'sortable' => 1,
-        'rules' => 'required',
-        'rules_messages' => ['it'=>['required'=>'Nome Obbligatorio']],
-        'value'=>'..',
+     *               'col_size' => 6,
+     *               'sortable' => 1,
+     *               'rules' => 'required',
+     *               'rules_messages' => ['it'=>['required'=>'Nome Obbligatorio']],
+     *               'value'=>'..',
      */
-    public function fields(): array {
-        return array (
-  0 => 
-  (object) array(
-     'type' => 'Id',
-     'name' => 'id',
-     'comment' => NULL,
-  ),
-  1 => 
-  (object) array(
-     'type' => 'Text',
-     'name' => 'name',
-     'rules' => 'required',
-     'comment' => NULL,
-  ),
-  2 => 
-  (object) array(
-     'type' => 'String',
-     'name' => 'created_by',
-     'comment' => NULL,
-  ),
-  3 => 
-  (object) array(
-     'type' => 'String',
-     'name' => 'updated_by',
-     'comment' => NULL,
-  ),
-  4 => 
-  (object) array(
-     'type' => 'String',
-     'name' => 'deleted_by',
-     'comment' => NULL,
-  ),
-  5 => 
-  (object) array(
-     'type' => 'Datetime',
-     'name' => 'created_at',
-     'comment' => NULL,
-  ),
-  6 => 
-  (object) array(
-     'type' => 'Datetime',
-     'name' => 'updated_at',
-     'comment' => NULL,
-  ),
-);
+    public function fields(): array
+    {
+        return [
+            0 => (object) [
+                'type' => 'Id',
+                'name' => 'id',
+                'comment' => null,
+            ],
+            1 => (object) [
+                'type' => 'Text',
+                'name' => 'name',
+                'rules' => 'required',
+                'comment' => null,
+            ],
+            2 => (object) [
+                'type' => 'String',
+                'name' => 'created_by',
+                'comment' => null,
+            ],
+            3 => (object) [
+                'type' => 'String',
+                'name' => 'updated_by',
+                'comment' => null,
+            ],
+            4 => (object) [
+                'type' => 'String',
+                'name' => 'deleted_by',
+                'comment' => null,
+            ],
+            5 => (object) [
+                'type' => 'Datetime',
+                'name' => 'created_at',
+                'comment' => null,
+            ],
+            6 => (object) [
+                'type' => 'Datetime',
+                'name' => 'updated_at',
+                'comment' => null,
+            ],
+        ];
     }
 
     /**
      * Get the tabs available.
-     *
-     * @return array
      */
-    public function tabs():array {
+    public function tabs(): array
+    {
         $tabs_name = [];
 
         return $tabs_name;
@@ -165,39 +157,33 @@ class CountryPanel extends XotBasePanel {
 
     /**
      * Get the cards available for the request.
-     *
-     * @return array
      */
-    public function cards(Request $request):array {
+    public function cards(Request $request): array
+    {
         return [];
     }
 
     /**
      * Get the filters available for the resource.
-     *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return array
      */
-    public function filters(Request $request = null):array {
+    public function filters(Request $request = null): array
+    {
         return [];
     }
 
     /**
      * Get the lenses available for the resource.
-     *
-     * @return array
      */
-    public function lenses(Request $request):array {
+    public function lenses(Request $request): array
+    {
         return [];
     }
 
     /**
      * Get the actions available for the resource.
-     *
-     * @return array
      */
-    public function actions():array {
+    public function actions(): array
+    {
         return [];
     }
 }

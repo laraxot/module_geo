f8a1cc7ba55c70d50cf4d5de11b38b54c50fe4ac
Retype GitHub Action <retype@object.net>
2023-09-07 16:15:17 +0000

    Refreshes Retype-generated documentation.
    Process triggered by dade1987.

--- resources/views/livewire/address-field.blade.php
+++ resources/views/livewire/address-field.blade.php
@@ -1,9 +1,11 @@
-@if (count($select_list) === 0)
-    <input type="text" wire:model="form_data.{{ $name }}" class="form-control" value="">
-@else
-    <select class="form-select" wire:model="form_data.{{ $name }}" wire:change="selected">
-        @foreach ($select_list as $itemKey => $itemVal)
-            <option value="{{ $itemKey }}">{{ $itemVal }}</option>
-        @endforeach
-    </select>
-@endif
+<div>
+    @if (count($select_list) === 1 && $name != 'country')
+        <input type="text" wire:model="form_data.{{ $name }}" class="form-control" value="">
+    @else
+        <select class="form-select" wire:model="form_data.{{ $name }}" wire:change="selected">
+            @foreach ($select_list as $itemKey => $itemVal)
+                <option value="{{ $itemKey }}">{{ $itemVal }}</option>
+            @endforeach
+        </select>
+    @endif
+</div>
